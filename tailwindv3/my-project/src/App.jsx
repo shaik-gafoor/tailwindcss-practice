4b96378ce986837185bb0c89cbd11d38c9693d94
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-17 12:04:08 +0530

    Tailwind setup

---
+++ tailwindv3/my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-700">
+      <h1 className="text-xl font-bold underline text-white">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
