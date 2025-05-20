51a408294784fdb46c0c9a46b64f154c4a9e158b
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-20 22:28:04 +0530

    Commit test

--- tailwindv4/src/App.tsx
+++ tailwindv4/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="heading flex flex-col">Hello world!</h1>
     </div>
   );
 };
